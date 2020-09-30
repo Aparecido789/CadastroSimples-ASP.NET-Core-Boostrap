c232c2f90dbb178b64387496176afaf06fcf43f4
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2020-09-30 17:55:08 -0300

    Adicionando um tema Free do Bootstrap ao projeto

--- SIstemaCadastro/SIstemaCadastro/Controllers/ProdutosController.cs
+++ SIstemaCadastro/SIstemaCadastro/Controllers/ProdutosController.cs
@@ -11,7 +11,6 @@ using SIstemaCadastro.Models;
 
 namespace SIstemaCadastro.Controllers
 {
-    [Authorize]
     public class ProdutosController : Controller
     {
         private readonly ApplicationDbContext _context;
